30742103aed4bb4d77039a8930361a3458277d88
Andrew <awilom6175@greenvilleschools.us>
2017-08-29 10:09:59 -0400

    yeeeeeeeeeeeee Made a bunch of things

--- OldMacDonald2.java
+++ OldMacDonald2.java
@@ -10,26 +10,36 @@
 
 public class OldMacDonald2 
 {
-    
+    /**
+     * Sings the first line
+     */
     public static void eieio() 
     { 
         System.out.println("Old MacDonald had a farm, E I E I O.");
     }
 
-
+    
+    /**
+     * Sings the second line + the animal
+     */
     public static void hadA(String animal) 
     { 
         System.out.println("And on that farm he had a " + animal + ", E I E I O.");
     }
 
-
+    /**
+     * Sings the sounds of the animal, note the +sound per each line
+     * Sings two lines of sounds
+     */
     public static void withA(String sound) 
     { 
         System.out.println("With a " + sound + " " + sound +  " here and a " + sound + " " + sound +  " there.");
         System.out.println("Here a " + sound + ", there a " + sound + ", everywhere a " + sound + ", " + sound + ".");  
     }
 
-    
+    /**
+     * Sings the following verse in order with ordered params 
+     */
     public static void singIt(String animal, String sound) 
     {
         eieio();
@@ -39,7 +49,9 @@ public class OldMacDonald2
         System.out.println();
     }
 
-    
+    /**
+     * List the animals with noises that it will display 
+     */
     public static void main(String[] args) 
     {
         System.out.println("Old MacDonald Had A Farm\n"); // Give our        song a title
